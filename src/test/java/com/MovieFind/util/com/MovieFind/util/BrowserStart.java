7fd4b0122d101c2c67b784d4ce8f3101ac1f6772
Akash64n <53176877+Akash64n@users.noreply.github.com>
2023-02-15 02:11:07 +0530

    Update BrowserStart.java

--- src/test/java/com/MovieFind/util/com/MovieFind/util/BrowserStart.java
+++ src/test/java/com/MovieFind/util/com/MovieFind/util/BrowserStart.java
@@ -12,12 +12,10 @@ public class BrowserStart {
 
 	public static WebDriver browserStart(String name)
 	{
-		if(name.equalsIgnoreCase("chrome"))
-		{
-		driver=new ChromeDriver();
+		if(name.equalsIgnoreCase("chrome")){
+		        driver=new ChromeDriver();
 		}
-		else if(name.equalsIgnoreCase("firefox"))
-		{
+		else if(name.equalsIgnoreCase("firefox")){
 			driver=new FirefoxDriver();
 		}
 		driver.manage().window().maximize();
