f25d3e63d2d31adffa0ac4d0a9372f3ffde7b7f2
Akash64n <53176877+Akash64n@users.noreply.github.com>
2023-02-15 02:13:24 +0530

    Update WikiTest.java

--- src/test/java/com/MovieFind/Test/com/MovieFind/Test/WikiTest.java
+++ src/test/java/com/MovieFind/Test/com/MovieFind/Test/WikiTest.java
@@ -1,8 +1,6 @@
 package com.MovieFind.Test;
 
 import static org.testng.Assert.assertEquals;
-
-
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -21,17 +19,14 @@ public class WikiTest extends WikiSearch {
 	  
 	  @Test
 	  public void wikiMovieDetailsTest() throws InterruptedException {
-		  
 		   String movieName="Pushpa";
 		   String details=WikiSearch.wikiMovieDetails(driver,movieName);
-		   
 		   String[] wikiDetails = details.split(":");
 		   String Country=wikiDetails[0];
 		   String Date=wikiDetails[1];;
 			
 		   assertEquals(Country,"India");
-		   assertEquals(Date,"17 December 2021");
-		     
+		   assertEquals(Date,"17 December 2021");   
 	  }
 	
 	  @AfterMethod
